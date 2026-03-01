e727f9255f71fd3f939f55058d8a5279524b5b3a
dhanalaxmi <dhanalaxmidupakuntla@gmail.com>
2026-03-01 10:30:07 +0530

    backend

--- server.js
+++ server.js
@@ -25,18 +25,8 @@ app.use(limiter);
 // CORS Configuration
 const frontendUrls = process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : ['http://localhost:5173', 'http://localhost:5174'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (frontendUrls.indexOf(origin) !== -1) {
-      return callback(null, true);
-    }
-    return callback(new Error('CORS policy: Origin not allowed'));
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors())
+
 app.use(express.json());
 app.use(helmet());
 
