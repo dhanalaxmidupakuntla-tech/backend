b37e67c3fb89761c83d0d5b111f15831ea633979
dhanalaxmi <dhanalaxmidupakuntla@gmail.com>
2026-03-02 05:15:19 +0530

    backend

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ app.use(limiter);
 
 /* ================= CORS ================= */
 
-app.use(cors({
-  origin: ["http://localhost:5173"],
-  credentials: true
-}));
+app.use(cors());
 
 /* ================= MIDDLEWARE ================= */
 app.use(express.json());
